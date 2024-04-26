65b8f9dd02e81a41ff31cc7969d486b04334c314
DavidC <davidcardenasgonz@gmail.com>
2024-04-25 21:33:57 -0600

    Implement the smooth-scroll to the connect with me buttons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Hero.css";
 import profile_img from "../../assets/profile_img.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
   return (
@@ -16,7 +17,11 @@ const Hero = () => {
         iste praesentium libero.
       </p>
       <div className="hero-action">
-        <div className="hero-connect">Connect with me</div>
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me
+          </AnchorLink>
+        </div>
         <div className="hero-resume">My resume</div>
       </div>
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -72,7 +72,11 @@ const Navbar = () => {
           {menu === "contact" ? <img src={underline} alt="" /> : <></>}
         </li>
       </ul>
-      <div className="nav-connect">Connect With Me</div>
+      <div className="nav-connect">
+        <AnchorLink className="anchor-link" offset={50} href="#contact">
+          Connect With Me
+        </AnchorLink>
+      </div>
     </div>
   );
 };
