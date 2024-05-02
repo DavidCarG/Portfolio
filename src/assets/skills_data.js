5c364b14ab94e03931bd28a6da621e9f9501fd5c
DavidC <davidcardenasgonz@gmail.com>
2024-05-02 13:29:55 -0600

    Refactor the services section into the skills section

---
+++ src/assets/skills_data.js
@@ -0,0 +1,43 @@
+/*
+Clerk : I use it for authentication
+Shadcn/ui : The components library I use
+Github : The tool I use for version control
+C++ : I have experience with this language due to competitive programming
+Python : I have experience with this language due to scripts I've written for personal use on my machine
+Linux Ubuntu : This is the OS I use, and so I have experience with linux
+*/
+
+const Skills_data = [
+  {
+    s_no: "01",
+    s_name: "Authentication",
+    s_desc: "Experience using Clerk for user authentication",
+  },
+  {
+    s_no: "02",
+    s_name: "UI Component Library",
+    s_desc: "Proficient in using Shadcn/ui for building user interfaces",
+  },
+  {
+    s_no: "03",
+    s_name: "Version Control",
+    s_desc: "Utilizes Git and GitHub for code management",
+  },
+  {
+    s_no: "04",
+    s_name: "C++ Programming",
+    s_desc: "Experience gained through competitive programming",
+  },
+  {
+    s_no: "05",
+    s_name: "Python Scripting",
+    s_desc: "Comfortable writing Python scripts for personal automation tasks",
+  },
+  {
+    s_no: "06",
+    s_name: "Linux (Ubuntu)",
+    s_desc: "Experience using Linux Ubuntu as the primary operating system",
+  },
+];
+
+export default Skills_data;
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,27 +1,26 @@
 import React from "react";
-import "./Services.css";
+import "./Skills.css";
 import theme_pattern from "../../assets/theme_pattern.svg";
-import Services_Data from "../../assets/services_data";
-import arrow_icon from "../../assets/arrow_icon.svg";
+import Skills_data from "../../assets/skills_data";
 
-const Services = () => {
+const Skills = () => {
   return (
     <div id="services" className="services">
       <div className="services-title">
-        <h1>My services</h1>
+        <h1>Other skills</h1>
         <img src={theme_pattern} alt="" />
       </div>
       <div className="services-container">
-        {Services_Data.map((service, index) => {
+        {Skills_data.map((service, index) => {
           return (
             <div key={index} className="services-format">
               <h3>{service.s_no}</h3>
               <h2>{service.s_name}</h2>
               <p>{service.s_desc}</p>
-              <div className="services-readmore">
+              {/* <div className="services-readmore">
                 <p>Read More</p>
                 <img src={arrow_icon} alt="" />
-              </div>
+              </div> */}
             </div>
           );
         })}
@@ -30,4 +29,4 @@ const Services = () => {
   );
 };
 
-export default Services;
+export default Skills;
