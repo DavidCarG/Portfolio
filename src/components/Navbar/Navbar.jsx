39d7bec5d6e6569d6c1ca74cf18b5b54ed7fa55d
DavidC <davidcardenasgonz@gmail.com>
2024-05-10 21:36:24 -0600

    Update Navbar styling and logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import "./Navbar.css";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/logo.png";
 import menu_open from "../../assets/menu_open.svg";
 import menu_close from "../../assets/menu_close.svg";
 import underline from "../../assets/nav_underline.svg";
@@ -20,7 +20,7 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
-      <img src={logo} alt="" />
+      <img src={logo} alt="" id="logo" />
       <img src={menu_open} onClick={openMenu} alt="" className="nav-mob-open" />
       <ul ref={menuRef} className="nav-menu">
         <img
