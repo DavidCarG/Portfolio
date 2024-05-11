415c67f5276961a820b497f657ca9923d98e6163
DavidC <davidcardenasgonz@gmail.com>
2024-05-10 21:36:17 -0600

    Update Footer component styling and logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.css";
-import footer_logo from "../../assets/footer_logo.svg";
+import footer_logo from "../../assets/logo.png";
 import user_icon from "../../assets/user_icon.svg";
 
 const Footer = () => {
@@ -8,17 +8,17 @@ const Footer = () => {
     <div className="footer">
       <div className="footer-top">
         <div className="footer-top-left">
-          <img src={footer_logo} alt="" />
+          <img id="logo" src={footer_logo} alt="" />
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni modi
-            sequi harum! Porro in et a, veritatis vero magnam? Libero eveniet
-            dicta corrupti, aliquid ducimus sint! Quisquam nemo eveniet rem.
+            Thank you for visiting my portfolio! I'm always open to new
+            opportunities and collaborations. Stay connected to see my journey
+            and updates.
           </p>
         </div>
         <div className="footer-top-right">
           <div className="footer-email-input">
             <img src={user_icon} alt="" />
-            <input type="email" placeholder="Enter your email" />
+            <input type="email" placeholder="Stay updated on my journey" />
           </div>
           <div className="footer-subscribe">Subscribe</div>
         </div>
@@ -26,12 +26,18 @@ const Footer = () => {
       <hr />
       <div className="footer-bottom">
         <div className="footer-bottom-left">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Based in Coahuila, México. Available for freelance projects.
         </div>
         <div className="footer-bottom-right">
-          <p>Term of Services</p>
-          <p>Privacy Policy</p>
-          <p>Connect with me</p>
+          <p>
+            <a href="terms.html">Term of Services</a>
+          </p>
+          <p>
+            <a href="privacy.html">Privacy Policy</a>
+          </p>
+          <p>
+            <a href="contact.html">Connect with me</a>
+          </p>
         </div>
       </div>
     </div>
